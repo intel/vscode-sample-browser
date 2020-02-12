535bdaabd18db6d584370ed97ef2a18af752cead
Thomas Ingleby <thomas.ingleby@intel.com>
2020-02-12 09:09:18 -0800

    fixup extension.ts
    Signed-off-by: Thomas Ingleby <thomas.ingleby@intel.com>

--- src/extension.ts
+++ src/extension.ts
@@ -9,15 +9,15 @@ import * as vscode from 'vscode';
 import { SampleProvider, SampleTreeItem } from './sampleData';
 import {SampleContainer} from './oneapicli';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void{
 		const sampleData = new SampleProvider();
-		//vscode.window.registerTreeDataProvider('upmLibs', upmData);
-		vscode.window.createTreeView("intel.oneAPISamples.tree", {treeDataProvider: sampleData, showCollapseAll: true});
-		vscode.commands.registerCommand('intel.oneAPISamples.create', (sample: SampleTreeItem) => sampleData.create(sample));
-		vscode.commands.registerCommand('intel.oneAPISamples.show', (sample: SampleContainer) => sampleData.show(sample));
-		vscode.commands.registerCommand('intel.oneAPISamples.clean', () => sampleData.clean());
-		vscode.commands.registerCommand('intel.oneAPISamples.refresh', () => sampleData.refresh());
-
+		context.subscriptions.push(vscode.window.createTreeView("intel.oneAPISamples.tree", {treeDataProvider: sampleData, showCollapseAll: true}));
+		context.subscriptions.push(vscode.commands.registerCommand('intel.oneAPISamples.create', (sample: SampleTreeItem) => sampleData.create(sample)));
+		context.subscriptions.push(vscode.commands.registerCommand('intel.oneAPISamples.show', (sample: SampleContainer) => sampleData.show(sample)));
+		context.subscriptions.push(vscode.commands.registerCommand('intel.oneAPISamples.clean', () => sampleData.clean()));
+		context.subscriptions.push(vscode.commands.registerCommand('intel.oneAPISamples.refresh', () => sampleData.refresh()));
 }
 
-export function deactivate() {}
+export function deactivate(): void {
+	console.log("Intel oneAPI Sample Browser: Goodbye");
+}
