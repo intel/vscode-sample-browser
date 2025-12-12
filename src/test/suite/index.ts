51cb1821d1de5c028301a658d3e995e38ff8d3fc
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2025-12-12 18:02:41 +0530

    Code merge for 2025.3.1 patch release (#44)

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd'
@@ -10,28 +10,26 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, '..');
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
+    try {
+        // glob v11 returns a Promise<string[]>
+        const files = await glob('**/**.test.js', { cwd: testsRoot });
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+        // Add files to the test suite
+        files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                console.error(err);
-                e(err);
-            }
+        // Run mocha tests inside a Promise
+        await new Promise<void>((resolve, reject) => {
+            mocha.run(failures => {
+                if (failures > 0) {
+                    reject(new Error(`${failures} tests failed.`));
+                } else {
+                    resolve();
+                }
+            });
         });
-    });
+
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
